49d59b1f88a24747b6032c1bfabb56da82992328
RocioSulca <rociosulca.z@gmail.com>
2021-06-10 13:12:30 -0500

    segundo commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,49 @@
 const cipher = {
- encode: function(offsetC,stringC){
-     // Convertiendo los numeros a codigo ASCII 
-  let asciiNumber = [];
-  
-  for (let i = 0; i < stringC.length; i++) {
-    asciiNumber[i] = stringC.charAt(i).charCodeAt(0);
-  }
-  console.log(asciiNumber);
+  encode: function (offsetC, stringC) {
+    if (offsetC === null || offsetC === 0) {
+      throw new TypeError();
+    }
+    // ASCII
+    let letraC;
+    let cadenaC = ""
+    for (let i = 0; i < stringC.length; i++) {
+      let ascii = stringC[i].charCodeAt(0);
+      if (ascii >= 65 && ascii <= 90) {
+        let letra = (ascii - 65 + parseInt(offsetC)) % 26 + 65;
+        letraC = String.fromCharCode(letra);
+      } /*else if (ascii >= 97 && ascii <= 122) {
+        let letra = (ascii - 97 + parseInt(offsetC)) % 26 + 97;
+        letraC = String.fromCharCode(letra);
+      }*/
+      for (let i = 0; i < letraC.length; i++) {
+        cadenaC += letraC
+      }
+    }
+    return cadenaC
+  },
 
- //Entrando dentro del array para poder sumarle el desplazamiento offset
-  for (let j = 0; j < asciiNumber.length; j++) {
-    let unaLetra = (asciiNumber[j] - 65 + parseInt(offsetC)) % 26 + 65;
-    let cambiandoUnaLetra = String.fromCharCode(unaLetra);
-    //codedText.innerHTML= cambiandoUnaLetra;
-    console.log(cambiandoUnaLetra);
+  decode: function (offsetD, stringD) {
+    if (offsetD === null || offsetD === 0) {
+      throw new TypeError();
+    }
+    // ASCII
+    let letraD;
+    let cadenaD = ""
+    for (let i = 0; i < stringD.length; i++) {
+      let asciiD = stringD[i].charCodeAt(0);
+      if (asciiD >= 65 && asciiD <= 90) {
+        let letras = (asciiD + 65 - parseInt(offsetD)) % 26 + 65;
+        letraD = String.fromCharCode(letras);
+      } /*else if (asciiD >= 97 && asciiD <= 122) {
+        let letras = (asciiD + 97 - parseInt(offsetD)) % 26 + 97;
+        letraD = String.fromCharCode(letras);
+      }*/
+      for (let i = 0; i < letraD.length; i++) {
+        cadenaD += letraD
+      }
+    }
+    return cadenaD
   }
-  //return asciiNumber;
- }
 };
-
-
-
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,66 +1,53 @@
 import cipher from './cipher.js';
 
-document.getElementById("code").addEventListener("click", coded/*(1,2)*/);
-function coded(/*a,b*/) {
+document.getElementById("code").addEventListener("click", () =>{
+
   document.getElementById("1screen").style.display = "none";
   document.getElementById("2screen").style.display = "block";
-  /*alert(a);
-  alert(b);*/
-}
-
-document.getElementById("decode").addEventListener("click", decoded);
-function decoded() {
+});
+document.getElementById("decode").addEventListener("click", () =>{
   document.getElementById("1screen").style.display = "none";
   document.getElementById("3screen").style.display = "block";
-}
-document.getElementById("back1").addEventListener("click", back);
-function back() {
+});
+document.getElementById("back1").addEventListener("click", () =>{
   document.getElementById("2screen").style.display = "none";
   document.getElementById("1screen").style.display = "block";
-}
-document.getElementById("back2").addEventListener("click", toBack);
-function toBack() {
+});
+document.getElementById("back2").addEventListener("click", () =>{
   document.getElementById("3screen").style.display = "none";
   document.getElementById("1screen").style.display = "block";
-}
-document.getElementById("clean").addEventListener("click", clean);
-function clean() {
+});
+document.getElementById("clean1").addEventListener("click", () =>{
   textToCode.value = ""
   codedText.innerHTML = ""
-  number.value = ""
-}
+  number1.value = ""
+});
+document.getElementById("clean2").addEventListener("click", () => {
+  textToDecode.value = ""
+  decodedText.innerHTML = ""
+  number2.value = ""
+});
 
 
+const textToCode = document.getElementById("textToCode");
+const codedText = document.getElementById("codedText");
+const number1 = document.getElementById("numero1");
 
-let textToCode = document.getElementById("textToCode");
-let codedText = document.getElementById("codedText");
-let number = document.getElementById("numero");
+document.getElementById("codify").addEventListener("click", function() {
+       
+    codedText.innerHTML = cipher.encode(number1.value, textToCode.value);
+});
 
-document.getElementById("codify").addEventListener("click", cambiar);
-function cambiar() {
-  //Moviendo el valor de un input a otro mediante el boton cambiar
-  let codificando = textToCode.value;
-  document.getElementById("codedText").value = codificando;
+ 
 
-  cipher.encode(number.value, textToCode.value);
-  codedText.innerHTML= cipher.cambiandoUnaLetra;
+const textToDecode = document.getElementById("textToDecode");
+const decodedText = document.getElementById("decodedText");
+const number2 = document.getElementById("numero2");
+
+document.getElementById("decodify").addEventListener("click", function() {
+  decodedText.innerHTML = cipher.decode(number2.value, textToDecode.value);
   
-}
+});
 
-
-
-
-
-
-/*let unaLetra= (parseInt(asciiNumber) - 65 + 3) %26 + 65
-let cambiandoUnaLetra = String.fromCharCode(unaLetra)
-codedText.innerHTML= cambiandoUnaLetra
-}*/
-
-
-/*let mayuscula= document.getElementById("textToCode").addEventListener("keyup", mayuscula)
-function mayuscula() {
-  this.value = this.value.toUpperCase();
-}*/
 
 console.log(cipher);
